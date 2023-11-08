c8a5ab3158ab500b1ba2268ca7b56403e18763d1
pcsishun <pcsishun@gmail.com>
2023-11-08 12:10:57 +0700

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,23 @@
 <script setup>
+  import VueCookies from 'vue-cookies'
+  import { useRouter } from 'vue-router';
+  import {onMounted} from "vue";
 
+  const router = useRouter();
+
+  onMounted(() => {
+    const data = VueCookies.get("setDataGosoft")
+    try{
+      if(data.token){
+        router.push({path: "/nutrition"})
+      }else{
+        router.push({path: "/login"})
+      }
+    }catch(err){
+      router.push({path: "/login"})
+    }
+  
+  })
 </script>
 
 <template>
